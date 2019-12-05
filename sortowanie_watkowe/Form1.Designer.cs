29538993195a0eb311351d753e1c2ad4462efbd7
Filiponesco <fifigaw98@gmail.com>
2019-12-05 19:54:13 +0100

    upload

---
+++ sortowanie_watkowe/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace sortowanie_watkowe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblTime1 = new System.Windows.Forms.Label();
+            this.lblTime2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnStartMultiThread = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(52, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Porównanie Quick Sort\'a";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(34, 121);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(100, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Sort. zwykłe";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(12, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Podaj ilość liczb: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(135, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // lblTime1
+            // 
+            this.lblTime1.AutoSize = true;
+            this.lblTime1.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblTime1.Location = new System.Drawing.Point(30, 147);
+            this.lblTime1.Name = "lblTime1";
+            this.lblTime1.Size = new System.Drawing.Size(109, 21);
+            this.lblTime1.TabIndex = 7;
+            this.lblTime1.Text = "0h:0m:0s:0ms";
+            // 
+            // lblTime2
+            // 
+            this.lblTime2.AutoSize = true;
+            this.lblTime2.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblTime2.Location = new System.Drawing.Point(184, 147);
+            this.lblTime2.Name = "lblTime2";
+            this.lblTime2.Size = new System.Drawing.Size(109, 21);
+            this.lblTime2.TabIndex = 8;
+            this.lblTime2.Text = "0h:0m:0s:0ms";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(257, 62);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 9;
+            this.btnGenerate.Text = "Generuj";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 197);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 219);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox1_Paint);
+            // 
+            // btnStartMultiThread
+            // 
+            this.btnStartMultiThread.Location = new System.Drawing.Point(188, 121);
+            this.btnStartMultiThread.Name = "btnStartMultiThread";
+            this.btnStartMultiThread.Size = new System.Drawing.Size(100, 23);
+            this.btnStartMultiThread.TabIndex = 11;
+            this.btnStartMultiThread.Text = "Sort. Wątkowe";
+            this.btnStartMultiThread.UseVisualStyleBackColor = true;
+            this.btnStartMultiThread.Click += new System.EventHandler(this.btnStartMultiThread_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(338, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(341, 407);
+            this.listBox1.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(12, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 17);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Podaj zakres    (0 - ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(135, 82);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 14;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            this.errorProvider1.RightToLeft = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 428);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnStartMultiThread);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.lblTime2);
+            this.Controls.Add(this.lblTime1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblTime1;
+        private System.Windows.Forms.Label lblTime2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnStartMultiThread;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ sortowanie_watkowe/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace sortowanie_watkowe
+{
+    public partial class Form1 : Form
+    {
+        const int SEQUENTIAL_THRESHOLD = 5000;
+        List<int> Numbers;
+        int zakres;
+        Random rnd;
+        int w = 5;
+        int ile;
+        Stopwatch stopwatch1;
+        Stopwatch stopwatch2;
+        public Form1()
+        {
+            InitializeComponent();
+            rnd = new Random();
+            Numbers = new List<int>();
+            stopwatch1 = new Stopwatch();
+            stopwatch2 = new Stopwatch();
+            lblTime1.Text = "";
+            lblTime2.Text = "";
+            //ile = pictureBox1.Width / w;
+            timer1.Stop();
+            timer2.Stop();
+        }
+
+        private void PictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics canvas = e.Graphics;
+            int nextTo = 1;
+            if(Numbers.Count > pictureBox1.Width / w)
+            {
+                nextTo = Numbers.Count / (pictureBox1.Width / w);
+            }
+            for (int i = 0; i < Numbers.Count; i += nextTo)
+            {
+                int scaleH = Numbers[i];
+                if(zakres > pictureBox1.Height)
+                {
+                    scaleH = (int)(((float)Numbers[i] / (float)zakres) * pictureBox1.Height);
+                }
+                Brush rectColor = Brushes.White;
+                Pen border = new Pen(Color.Black);
+                Rectangle rect = new Rectangle
+                    (w * (i / nextTo), pictureBox1.Height - scaleH, w, scaleH);
+                canvas.FillRectangle
+                    (rectColor, w * (i / nextTo), (pictureBox1.Height - scaleH), w, scaleH);
+                canvas.DrawRectangle(border, rect);
+            }
+        }
+        private void Sortuj()
+        {
+            Numbers.Sort();
+        }
+        private void QuickSort(int start, int end)
+        {
+            if (start >= end) return;
+            int index = Partition(start, end);
+            QuickSort(start, index - 1);
+            QuickSort(index + 1, end);
+        }
+        private int Partition(int start, int end)
+        {
+            int pivotIndex = start; //to guess
+            float pivotValue = Numbers[end];
+            for (int i = start; i < end; i++)
+            {
+                if (Numbers[i] < pivotValue)
+                {
+                    Swap(i, pivotIndex);
+                    pivotIndex++;
+                }
+            }
+            Swap(pivotIndex, end);
+            return pivotIndex;
+        }
+        private void Swap(int a, int b)
+        {
+            var pom = Numbers[a];
+            Numbers[a] = Numbers[b];
+            Numbers[b] = pom;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ShowTime(stopwatch1, lblTime1);
+        }
+        private void ShowTime(Stopwatch sw, object sender)
+        {
+            var lbl = (Label)sender;
+            lbl.Text = String.Format
+                ("{0,00}h:{1,00}m:{2,00}s:{3,00}ms", sw.Elapsed.Hours, sw.Elapsed.Minutes, sw.Elapsed.Seconds, sw.Elapsed.Milliseconds / 10);
+        }
+        private void GenerateNumbers()
+        {
+                zakres = int.Parse(textBox2.Text);
+                Numbers.Clear();
+                for (long i = 0; i < ile; i++)
+                {
+                    Numbers.Add(rnd.Next(zakres));
+                }
+        }
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            errorProvider1.Clear();
+            try
+            {
+                ChangeEnabledBtn(btnStart, true);
+                ChangeEnabledBtn(btnStartMultiThread, true);
+                ile = int.Parse(textBox1.Text);
+                GenerateNumbers();
+                pictureBox1.Refresh();
+            }
+            catch (Exception ex)
+            {
+                errorProvider1.SetError(btnGenerate, ex.Message);
+            }
+        }
+
+        async private void btnStart_Click(object sender, EventArgs e)
+        {
+            ChangeEnabledBtn(sender, false);
+            ChangeEnabledBtn(btnStartMultiThread, false);
+            Task task = new Task(() => QuickSort(0, Numbers.Count - 1));
+            stopwatch1.Restart();
+            stopwatch1.Start();
+            timer1.Start();
+            task.Start();
+            //czeka na watek ale tym samym nie blokuje interfejsu
+            await task;
+            stopwatch1.Stop();           
+            ShowOnList(stopwatch1, "Zwykłe");
+            timer1.Stop();
+            pictureBox1.Refresh();
+        }
+        private void QuickSortMultiThread(int start, int end)
+        {
+            if (start >= end) return;
+            if((end - start) < SEQUENTIAL_THRESHOLD)
+            {
+                QuickSort(start, end);
+            }
+            else
+            {
+                int index = Partition(start, end);
+                //metoda Invoke czeka aż zakończą pracę
+                Parallel.Invoke(
+                () => QuickSortMultiThread(start, index - 1),
+                () => QuickSortMultiThread(index + 1, end));
+            }
+        }
+
+        async private void btnStartMultiThread_Click(object sender, EventArgs e)
+        {
+            ChangeEnabledBtn(sender, false);
+            ChangeEnabledBtn(btnStart, false);
+            Task task = new Task( () => QuickSortMultiThread(0, Numbers.Count - 1));
+            stopwatch2.Restart();
+            stopwatch2.Start();
+            timer2.Start();
+            task.Start();
+            //zaczekaj na wątek
+            await task;
+            stopwatch2.Stop();
+            ShowOnList(stopwatch2, "Wątkowe");
+            timer2.Stop();
+            pictureBox1.Refresh();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            ShowTime(stopwatch2, lblTime2);
+        }
+        private void ChangeEnabledBtn(object obj, bool b)
+        {
+            var btn = (Button)obj;
+            btn.Enabled = b;
+        }
+        private void ShowOnList(Stopwatch sw, string text)
+        {
+            listBox1.Items.Add
+                (String.Format
+                (text+ " Ilość: {0}, zakres: {1}, czas: {2, 00}h:{3, 00}m:{4, 00}s:{5,00}ms",
+                Numbers.Count, zakres, sw.Elapsed.Hours,
+                sw.Elapsed.Minutes, sw.Elapsed.Seconds, sw.Elapsed.Milliseconds / 10));
+        }
+    }
+}
